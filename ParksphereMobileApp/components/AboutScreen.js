86ffa881754f1e6c06a30cec1b8db3686f2c7963
dinos3741 <dinos.dimou@gmail.com>
2025-11-21 17:48:12 +0200

    refactor(AboutScreen): update back arrow icon for a lighter appearance

--- ParksphereMobileApp/components/AboutScreen.js
+++ ParksphereMobileApp/components/AboutScreen.js
@@ -7,7 +7,7 @@ const AboutScreen = ({ onClose }) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.backButton} onPress={onClose}>
-        <FontAwesome name="arrow-left" size={24} color="#4dd0e1" />
+        <FontAwesome name="chevron-left" size={24} color="#4dd0e1" />
       </TouchableOpacity>
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <Text style={styles.title}>What is ParkSphere?</Text>
