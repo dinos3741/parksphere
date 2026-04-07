1441f6ac746b0454b2382413344c027be562074e
dinos3741 <dinos.dimou@gmail.com>
2026-04-08 01:03:57 +0300

    fix: force useProxy in Google auth request

--- ParksphereMobileApp/components/Register.js
+++ ParksphereMobileApp/components/Register.js
@@ -21,8 +21,10 @@ const Register = ({ onBack, onLogin }) => {
     webClientId: '320058445002-lddk8d48h06bei48bh6u08ku97t1i3kd.apps.googleusercontent.com',
     iosClientId: '320058445002-oo08jes63ti9rtqkhpo9d1jfi6fcoo31.apps.googleusercontent.com',
     androidClientId: '320058445002-lddk8d48h06bei48bh6u08ku97t1i3kd.apps.googleusercontent.com',
-  }, {
-    projectNameForProxy: 'ParksphereMobileApp',
+    redirectUri: AuthSession.makeRedirectUri({
+      useProxy: true,
+      projectNameForProxy: 'ParksphereMobileApp',
+    }),
   });
 
   useEffect(() => {
